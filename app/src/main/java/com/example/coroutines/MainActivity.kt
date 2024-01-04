c2297dafee935307755585186ff05e6a2536368e
Kaustubh <kaustubhbhagwat93@gmail.com>
2024-01-04 18:20:02 +0530

    Coroutine Context

--- app/src/main/java/com/example/coroutines/MainActivity.kt
+++ app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -4,21 +4,32 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import com.example.coroutines.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     val TAG = "MainActivity"
+    private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        GlobalScope.launch {
-            delay(1000L)
-          val ans =  doNetworkCall()
-            Log.d(TAG,ans)
+        GlobalScope.launch(Dispatchers.IO) {
+            Log.d(TAG,"Starting Coroutine in thread ${Thread.currentThread().name}")
+            val ans = doNetworkCall()
+            withContext(Dispatchers.Main) {
+                Log.d(TAG,"Starting Coroutine in thread ${Thread.currentThread().name}")
+                binding.tvDummy.text = "Hello Kaustubh"
+            }
+            Log.d(TAG, ans)
         }
     }
 
