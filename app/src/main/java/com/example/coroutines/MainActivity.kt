4fe60914b1343b4ac4c1e772d340e3433867b9de
Kaustubh <kaustubhbhagwat93@gmail.com>
2024-01-06 19:49:51 +0530

    Jobs and cancel

--- app/src/main/java/com/example/coroutines/MainActivity.kt
+++ app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -8,10 +8,12 @@ import com.example.coroutines.databinding.ActivityMainBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.newSingleThreadContext
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
+import kotlinx.coroutines.withTimeout
 
 class MainActivity : AppCompatActivity() {
     val TAG = "MainActivity"
@@ -23,32 +25,60 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        GlobalScope.launch(Dispatchers.IO) {
-            Log.d(TAG,"Starting Coroutine in thread ${Thread.currentThread().name}")
-            val ans = doNetworkCall()
-            withContext(Dispatchers.Main) {
-                Log.d(TAG,"Starting Coroutine in thread ${Thread.currentThread().name}")
-                binding.tvDummy.text = "Hello Kaustubh"
-            }
-            Log.d(TAG, ans)
-        }
+//        GlobalScope.launch(Dispatchers.IO) {
+//            Log.d(TAG,"Starting Coroutine in thread ${Thread.currentThread().name}")
+//            val ans = doNetworkCall()
+//            withContext(Dispatchers.Main) {
+//                Log.d(TAG,"Starting Coroutine in thread ${Thread.currentThread().name}")
+//                binding.tvDummy.text = "Hello Kaustubh"
+//            }
+//            Log.d(TAG, ans)
+//        }
+//
+//        Log.d(TAG,"Before runblocking")
+//        runBlocking {
+//            launch(Dispatchers.IO) {
+//                delay(3000L)
+//                Log.d(TAG,"Finished IO coroutine 1")
+//            }
+//            launch(Dispatchers.IO) {
+//                delay(3000L)
+//                Log.d(TAG,"Finished IO coroutine 2")
+//            }
+//            Log.d(TAG,"Start of runblocking")
+//            delay(5000L)
+//            Log.d(TAG,"End of runblocking")
+//        }
+//
+//        Log.d(TAG,"After runblocking")
 
-        Log.d(TAG,"Before runblocking")
+        val job = GlobalScope.launch(Dispatchers.Default) {
+//            repeat(5){
+//                Log.d(TAG,"Coroutine is still working")
+//                delay(1000L)
+//            }
+            Log.d(TAG,"Starting long running calculation")
+            withTimeout(3000L){
+                for(i in 30..40){
+                    if(isActive){
+                        Log.d(TAG,"Result for i =$i: ${fib(i)}")
+                    }
+                }
+            }
+            Log.d(TAG,"Ending long running calculation")
+        }
         runBlocking {
-            launch(Dispatchers.IO) {
-                delay(3000L)
-                Log.d(TAG,"Finished IO coroutine 1")
-            }
-            launch(Dispatchers.IO) {
-                delay(3000L)
-                Log.d(TAG,"Finished IO coroutine 2")
-            }
-            Log.d(TAG,"Start of runblocking")
-            delay(5000L)
-            Log.d(TAG,"End of runblocking")
+            delay(2000L)
+            job.cancel()
+            Log.d(TAG, "Cancelled Job")
         }
+    }
 
-        Log.d(TAG,"After runblocking")
+    fun fib(n:Int): Long{
+        return if(n==0) 0
+        else if (n==1) 1
+        else fib(n-1) + fib(n-2)
+
     }
 
     suspend fun doNetworkCall(): String {
