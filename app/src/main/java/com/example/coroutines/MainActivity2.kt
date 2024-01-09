82bad79bc18f2dfb4179195250671de4d84ad0ef
Kaustubh <kaustubhbhagwat93@gmail.com>
2024-01-09 19:52:18 +0530

    lifecycleScope

--- app/src/main/java/com/example/coroutines/MainActivity2.kt
+++ app/src/main/java/com/example/coroutines/MainActivity2.kt
@@ -1,9 +1,12 @@
 package com.example.coroutines
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings.Global
 import android.util.Log
+import androidx.lifecycle.lifecycleScope
+import com.example.coroutines.databinding.ActivityMain2Binding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
@@ -15,10 +18,30 @@ import kotlin.time.measureTime
 class MainActivity2 : AppCompatActivity() {
 
     val TAG = "MainActivity2"
+    private lateinit var binding: ActivityMain2Binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+
+            lifecycleScope.launch {
+                while (true){
+                    delay(1000L)
+                    Log.d(TAG,"Still running")
+                }
+            }
+
+            GlobalScope.launch {
+                delay(5000L)
+                Intent(this@MainActivity2,MainActivity3::class.java).also {
+                    startActivity(it)
+                    finish()
+                }
+            }
+        }
 
         GlobalScope.launch(Dispatchers.IO) {
 
